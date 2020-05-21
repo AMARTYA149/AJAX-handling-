e1bd0d081acc03ccb311bc06850199199c2090e3
Amartya Aishwarya <amartya970914@gmail.com>
2020-05-21 15:19:13 +0530

    error ajax handling vanilla js method

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 function fetchRandomDogImage() {
     // Javascript way of Ajax request handling
-    // var xhrRequest = new XMLHttpRequest();
+    var xhrRequest = new XMLHttpRequest();
 
-    // xhrRequest.onload = function(){
-    //     console.log(xhrRequest.response);
-    //     var responseJSON = JSON.parse(xhrRequest.response);
-    //     var imageURL = responseJSON.message;
-    //     $('#dog-image').attr('src', imageURL);
-    // };
-    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
-    // xhrRequest.send();
+    xhrRequest.onload = function(){
+        console.log(xhrRequest.response);
+        var responseJSON = JSON.parse(xhrRequest.response);
+        var imageURL = responseJSON.message;
+        $('#dog-image').attr('src', imageURL);
+    };
+    xhrRequest.onerror = function(){
+        console.log("request failed");
+    };
+    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    xhrRequest.send();
 
     // jQuery way of handling Ajax - method 1
-    $.ajax({
-        url: 'https://dog.ceo/api/breeds/image/random',
-        method: 'get',
-        success: function(data){
-             //     var responseJSON = JSON.parse(xhrRequest.response);
-        var imageURL = data.message;
-        $('#dog-image').attr('src', imageURL);
-        }
-    }).fail(function(){
-        console.log("error");
-    });
+    // $.ajax({
+    //     url: 'https://dog.ceo/api/breeds/image/random',
+    //     method: 'get',
+    //     success: function(data){
+    //          //     var responseJSON = JSON.parse(xhrRequest.response);
+    //     var imageURL = data.message;
+    //     $('#dog-image').attr('src', imageURL);
+    //     }
+    // }).fail(function(){
+    //     console.log("error");
+    // });
 
     //jQuery way of handling Ajax - method 2
     // $.get('https://dog.ceo/api/breeds/image/random',  function(data){        
