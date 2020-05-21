5815c07570bbb933c5d12c22c1497e10e5b4d29a
Amartya Aishwarya <amartya970914@gmail.com>
2020-05-21 14:44:44 +0530

    basic jquery handling method - 1

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function fetchRandomDogImage() {
-    var xhrRequest = new XMLHttpRequest();
+    // Javascript way of Ajax request handling
+    // var xhrRequest = new XMLHttpRequest();
 
-    xhrRequest.onload = function(){
-        console.log(xhrRequest.response);
-        var responseJSON = JSON.parse(xhrRequest.response);
-        var imageURL = responseJSON.message;
+    // xhrRequest.onload = function(){
+    //     console.log(xhrRequest.response);
+    //     var responseJSON = JSON.parse(xhrRequest.response);
+    //     var imageURL = responseJSON.message;
+    //     $('#dog-image').attr('src', imageURL);
+    // };
+    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    // xhrRequest.send();
+
+    // jQuery way of handling Ajax - method 1
+    $.ajax({
+        url: 'https://dog.ceo/api/breeds/image/random',
+        method: 'get',
+        success: function(data){
+             //     var responseJSON = JSON.parse(xhrRequest.response);
+        var imageURL = data.message;
         $('#dog-image').attr('src', imageURL);
-    };
-    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
-    xhrRequest.send();
-
-
+        }
+    });
 }
 
 $('#fetch-dog-image-button').click(fetchRandomDogImage);
